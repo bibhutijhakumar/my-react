fa024532047f684b21a8301b35cd2fdb29f54813
Bibhuti <bibhuti@bold.com>
2024-11-04 21:07:58 +0530

    my 2nd commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>My React Learning</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>My React Learning with Vite | BJ</h1>
+  )
+}
+
+export default App
